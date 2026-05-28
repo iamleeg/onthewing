e2d7692625c0fe605a313c833adfa8d365118ba1
Graham Lee <grahamlee@acm.org>
2026-05-28 14:45:32 +0100

    Explicitly synthesize OTWStyleSheet.stylesheetName and add AGPL3 notice
    Signed-off-by: Graham Lee <grahamlee@acm.org>

--- OTWStyleSheet.h
+++ OTWStyleSheet.h
@@ -1,7 +1,26 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// OTWStyleSheet.h - A component that adds a stylesheet from the app's resources.
+//
+// Copyright (C) 2026 Graham Lee
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 #import <WebObjects/WebObjects.h>
 
 @interface OTWStyleSheet : GSWComponent {
+    NSString *_stylesheetName;
 }
 
 @property (nonatomic, retain) NSString *stylesheetName;
--- OTWStyleSheet.m
+++ OTWStyleSheet.m
@@ -1,7 +1,27 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// OTWStyleSheet.m - A component that adds a stylesheet from the app's resources.
+//
+// Copyright (C) 2026 Graham Lee
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 #import "OTWStyleSheet.h"
 
 @implementation OTWStyleSheet
+
+@synthesize stylesheetName = _stylesheetName;
 
 - (void)appendToResponse:(WOResponse *)response inContext:(WOContext *)context {
     NSString *url = [[context application] urlForResourceNamed:self.stylesheetName
