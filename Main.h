c4c317dae00bd7f5d77b7b0d4b06678e19ff3801
Graham Lee <grahamlee@acm.org>
2026-07-30 16:42:59 +0100

    Add reference documentation for the Main component.
    Signed-off-by: Graham Lee <grahamlee@acm.org>

--- Main.h
+++ Main.h
@@ -24,19 +24,44 @@
 
 /**
  A component that renders the main user interface for the OnTheWing app.
+
  */
 @interface Main : GSWComponent {
 }
 
+/**
+ An action an observer calls to capture an observation.
+ Observations are "pending" in the Session until an observer reviews them in the ReviewObservations interface,
+ and either deletes them or adds them to their journal.
+ */
 - capture;
+/**
+ An action an observer calls to review their pending observations.
+ */
 - (id)reviewObservations;
+/**
+ An action an observer calls to browse their nature journal.
+ */
 - (id)browseJournal;
+/**
+ A Boolean value that indicates whether the observer has pending observations in their Session.
+ Returns YES if there are any pending observations; NO otherwise.
+ */
 - (BOOL)hasObservations;
+/**
+ A string that indicates to an observer whether they have pending observations in their Session.
+ */
 - (NSString *)reportPendingObservations;
+/**
+ A Boolean value that indicates whether the Session is associated with an observer.
+ Returns YES if an Observer is authenticated in the Session; NO otherwise.
+ */
 - (BOOL)hasUser;
 
-// Whether the app's DB connection is ready.
-// Main shows a friendly "come back soon" message instead of the real UI when this is NO.
+/**
+ A Boolean value that indicates whether the OTWApp is ready to receive external requests.
+ This component displays a placeholder UI when this method returns NO.
+ */
 - (BOOL)isAppReady;
 
 @end
