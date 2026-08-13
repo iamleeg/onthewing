4ee32f8d0852259a7ccce372accf55a7d36f4b0d
Graham Lee <grahamlee@acm.org>
2026-08-13 16:58:37 +0100

    Validate the stripe webhook signing.
    Signed-off-by: Graham Lee <grahamlee@acm.org>

--- DirectAction.m
+++ DirectAction.m
@@ -25,6 +25,8 @@
 #import "OTWFlashMessage.h"
 #import "Observer.h"
 #import <EOControl/EOControl.h>
+#include <gnutls/gnutls.h>
+#include <gnutls/crypto.h>
 
 @implementation DirectAction
 
@@ -51,6 +53,53 @@
     NSData *body = [req content];
     
     NSError *error = nil;
+    NSString *secret = [[[NSProcessInfo processInfo] environment] objectForKey:@"STRIPE_WEBHOOK_SECRET"];
+    if (secret && [secret length] > 0) {
+        NSString *signatureHeader = [req headerForKey:@"Stripe-Signature"];
+        if (!signatureHeader) {
+            WOResponse *resp = [[[WOResponse alloc] init] autorelease];
+            [resp setStatus:400];
+            return resp;
+        }
+        
+        NSArray *components = [signatureHeader componentsSeparatedByString:@","];
+        NSString *timestamp = nil;
+        NSString *signature = nil;
+        for (NSString *comp in components) {
+            NSString *trimmed = [comp stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+            if ([trimmed hasPrefix:@"t="]) {
+                timestamp = [trimmed substringFromIndex:2];
+            } else if ([trimmed hasPrefix:@"v1="]) {
+                signature = [trimmed substringFromIndex:3];
+            }
+        }
+        
+        if (!timestamp || !signature) {
+            WOResponse *resp = [[[WOResponse alloc] init] autorelease];
+            [resp setStatus:400];
+            return resp;
+        }
+        
+        NSString *bodyStr = [[[NSString alloc] initWithData:body encoding:NSUTF8StringEncoding] autorelease];
+        NSString *signedPayloadStr = [NSString stringWithFormat:@"%@.%@", timestamp, bodyStr];
+        NSData *signedPayload = [signedPayloadStr dataUsingEncoding:NSUTF8StringEncoding];
+        NSData *keyData = [secret dataUsingEncoding:NSUTF8StringEncoding];
+        
+        unsigned char mac[32];
+        gnutls_hmac_fast(GNUTLS_MAC_SHA256, [keyData bytes], [keyData length], [signedPayload bytes], [signedPayload length], mac);
+        
+        NSMutableString *hexMac = [NSMutableString stringWithCapacity:64];
+        for(int i = 0; i < 32; i++) {
+            [hexMac appendFormat:@"%02x", mac[i]];
+        }
+        
+        if (![signature isEqualToString:hexMac]) {
+            WOResponse *resp = [[[WOResponse alloc] init] autorelease];
+            [resp setStatus:400];
+            return resp;
+        }
+    }
+
     NSDictionary *payload = [NSJSONSerialization JSONObjectWithData:body options:0 error:&error];
     if (!payload || error) {
         WOResponse *resp = [[[WOResponse alloc] init] autorelease];
